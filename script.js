c337dd9c4cf54cce0294db5e2a43d6ec0160061c
Nosovitski <nosovitski.gleb@gmail.com>
2021-06-29 22:20:44 +0300

    Test2

--- script.js
+++ script.js
@@ -1,8 +1,148 @@
+'use strict'
+
+const userLang = navigator.language || navigator.userLanguage; 
+const languages = {
+	en : {
+		"Unlimited Access<br>to All Features": "Unlimited Access<br>to All Features",
+		"Unlimited documents": "Unlimited documents",
+		"Count mode": "Count mode",
+		"Text recognition (OCR)": "Text recognition (OCR)",
+		"Monthly": "Monthly",
+		"<strong>{{price}}</strong><br>per month": "<strong>{{price}}</strong><br>per month",
+		"3 DAYS FREE": "3 DAYS FREE",
+		"{{price}}/month": "{{price}}/month",
+		"Annually": "Annually",
+		"-83%": "-83%",
+		"<strong>{{price}}</strong><br>per year": "<strong>{{price}}</strong><br>per year",
+		"MOST POPULAR": "MOST POPULAR",
+		"Continue": "Continue",
+		"Auto-renewable. Cancel anytime.": "Auto-renewable. Cancel anytime.",
+		"Terms of Use": "Terms of Use",
+		"Restore": "Restore",
+		"Privacy Policy": "Privacy Policy"
+		},
+	es : {
+		"Unlimited Access<br>to All Features": "Acceso ilimitado<br>a todas las funciones",
+		"Unlimited documents": "Documentos ilimitados",
+		"Count mode": "Modo de recuento",
+		"Text recognition (OCR)": "Reconocimiento de texto (OCR)",
+		"Monthly": "Mensual",
+		"<strong>{{price}}</strong><br>per month": "<strong>{{price}}</strong><br>por mes",
+		"3 DAYS FREE": "3 DÍAS GRATIS",
+		"{{price}}/month": "{{price}}/mes",
+		"Annually": "Anual",
+		"-83%": "-83%",
+		"<strong>{{price}}</strong><br>per year": "<strong>{{price}}</strong><br>por año",
+		"MOST POPULAR": "LO MÁS PEDIDO",
+		"Continue": "Continuar",
+		"Auto-renewable. Cancel anytime.": "Renovable automáticamente. Cancela cuando quieras.",
+		"Terms of Use": "Términos de uso",
+		"Restore": "Restablecer",
+		"Privacy Policy": "Política de privacidad"  
+		},
+	fr : {
+		"Unlimited Access<br>to All Features": "Accès illimité<br>à toutes les fonctionnalités",
+		"Unlimited documents": "Documents illimités",
+		"Count mode": "Mode décompte",
+		"Text recognition (OCR)": "Reconnaissance de texte (OCR)",
+		"Monthly": "Tous les mois",
+		"<strong>{{price}}</strong><br>per month": "<strong>{{price}}</strong><br>par mois",
+		"3 DAYS FREE": "3 JOURS GRATUITS",
+		"{{price}}/month": "{{price}}/mois",
+		"Annually": "Tous les ans",
+		"-83%": "-83%",
+		"<strong>{{price}}</strong><br>per year": "<strong>{{price}}</strong><br>par an",
+		"MOST POPULAR": "LE PLUS POPULAIRE",
+		"Continue": "Continuer",
+		"Auto-renewable. Cancel anytime.": "Renouvelable automatiquement. Annulez à tout moment.",
+		"Terms of Use": "Conditions d'utilisation",
+		"Restore": "Restaurer",
+		"Privacy Policy": "Politique de confidentialité"
+		},
+	ja : {
+		"Unlimited Access<br>to All Features": "全ての機能へ<br>無制限アクセス",
+		"Unlimited documents": "無制限のドキュメント",
+		"Count mode": "カウントモード",
+		"Text recognition (OCR)": "テキスト認識 (OCR)",
+		"Monthly": "月次",
+		"<strong>{{price}}</strong><br>per month": "<strong>{{price}}</strong><br>/月",
+		"3 DAYS FREE": "3日間無料",
+		"{{price}}/month": "{{price}}/か月",
+		"Annually": "年次",
+		"-83%": "-83%",
+		"<strong>{{price}}</strong><br>per year": "<strong>{{price}}</strong><br>/年",
+		"MOST POPULAR": "一番人気",
+		"Continue": "続行する",
+		"Auto-renewable. Cancel anytime.": "自動更新可能。いつでもキャンセル可能。",
+		"Terms of Use": "利用規約",
+		"Restore": "復元する",
+		"Privacy Policy": "プライバシーポリシー"
+		},
+	nl : {
+		"Unlimited Access<br>to All Features": "Onbeperkte toegang<br>tot alle functies",
+		"Unlimited documents": "Onbeperkt aantal documenten",
+		"Count mode": "Aantal modus",
+		"Text recognition (OCR)": "Gratis tekstherkenning (OCR)",
+		"Monthly": "Maandelijks",
+		"<strong>{{price}}</strong><br>per month": "<strong>{{price}}</strong><br>per maand",
+		"3 DAYS FREE": "3 DAGEN GRATIS",
+		"{{price}}/month": "{{price}}/maand",
+		"Annually": "Jaarlijks",
+		"-83%": "-83%",
+		"<strong>{{price}}</strong><br>per year": "<strong>{{price}}</strong><br>per jaar",
+		"MOST POPULAR": "MEEST POPULAIR",
+		"Continue": "Doorgaan",
+		"Auto-renewable. Cancel anytime.": "Automatisch verlengbaar. Altijd annuleren.",
+		"Terms of Use": "Gebruiksvoorwaarden",
+		"Restore": "Herstellen",
+		"Privacy Policy": "Privacybeleid"
+		},
+	ru : {
+		"Unlimited Access<br>to All Features": "Неограниченный доступ<br>ко всем функциям",
+		"Unlimited documents": "Любое количество документов",
+		"Count mode": "Режим «Подсчет»",
+		"Text recognition (OCR)": "Распознавание текста (OCR)",
+		"Monthly": "1 месяц",
+		"<strong>{{price}}</strong><br>per month": "<strong>{{price}}</strong><br>в месяц",
+		"3 DAYS FREE": "3 ДНЯ БЕСПЛАТНО",
+		"{{price}}/month": "{{price}}/месяц",
+		"Annually": "1 ГОД",
+		"-83%": "-83%",
+		"<strong>{{price}}</strong><br>per year": "<strong>{{price}}</strong><br>в год",
+		"MOST POPULAR": "ПОПУЛЯРНОЕ",
+		"Continue": "Продолжить",
+		"Auto-renewable. Cancel anytime.": "Автопродление. Отмена в любое время.",
+		"Terms of Use": "Условия использования",
+		"Restore": "Восстановить",
+		"Privacy Policy": "Конфиденциальность"
+		},
+	zh : {
+		"Unlimited Access<br>to All Features": "无限制使用<br>所有功能",
+		"Unlimited documents": "文档无限制",
+		"Count mode": "计数模式",
+		"Text recognition (OCR)": "文本识别（OCR）",
+		"Monthly": "每月",
+		"<strong>{{price}}</strong><br>per month": "<strong>{{price}}</strong>每月<br>",
+		"3 DAYS FREE": "3日免费",
+		"{{price}}/month": "{{price}}/月",
+		"Annually": "包年",
+		"-83%": "-83%",
+		"<strong>{{price}}</strong><br>per year": "每年<strong>{{price}}</strong><br>",
+		"MOST POPULAR": "最热门",
+		"Continue": "继续",
+		"Auto-renewable. Cancel anytime.": "自动续订。可随时取消。",
+		"Terms of Use": "使用条款",
+		"Restore": "恢复",
+		"Privacy Policy": "隐私政策"
+	},
+}
 
 
-var userLang = navigator.language || navigator.userLanguage; 
-console.log ( userLang );
+document.documentElement.lang = userLang.slice(0,2).toLowerCase()
 
-document.documentElement.lang = 'by'
 
-console.log(document.documentElement.lang)
+Object.keys(languages).forEach(lang => {
+	if (userLang.slice(0,2).toLowerCase() === lang) {
+		console.log(languages[lang])
+	}
+})
